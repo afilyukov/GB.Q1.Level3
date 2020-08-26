a519e7584e8de4379a00a846f1baf528f23c34d8
Alexander Filyukov <filyukov@yandex.ru>
2020-08-26 13:49:51 +0300

    Lesson02 done

---
+++ Lesson02/src/StartClients.java
@@ -0,0 +1,7 @@
+public class StartClients {
+    public static void main(String[] args) {
+        new Client1();
+        new Client2();
+        new Client3();
+    }
+}
---
+++ out/production/Lesson02/Commands.sql
@@ -0,0 +1,12 @@
+CREATE TABLE CLIENTS (
+    id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(15) NOT NULL,
+    login VARCHAR(15) NOT NULL,
+    password VARCHAR(15) NOT NULL
+
+    INSERT INTO `CLIENTS` (`id`, `name`, `login`, `password`) VALUES (NULL, 'Barboss', 'l1', 'p1');
+    INSERT INTO `CLIENTS` (`id`, `name`, `login`, `password`) VALUES (NULL, 'Kelvin', 'l2', 'p2');
+    INSERT INTO `CLIENTS` (`id`, `name`, `login`, `password`) VALUES (NULL, 'Nicky', 'l3', 'p3');
+    INSERT INTO `CLIENTS` (`id`, `name`, `login`, `password`) VALUES (NULL, 'Klaus', 'l4', 'p4');
+)
+
